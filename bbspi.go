01f68d43943ca23a3c0a90ff939cd21414cd382f
soypat <graded.sp@gmail.com>
2023-02-02 23:48:40 -0300

    add more code

---
+++ bbspi.go
@@ -0,0 +1,72 @@
+package cyw43439
+
+import "machine"
+
+// bbSPI is a dumb bit-bang implementation of SPI protocol that is hardcoded
+// to mode 0 and ignores trying to receive data. Just enough for the APA102.
+// Note: making this unexported for now because it is probable not suitable
+// most purposes other than the APA102 package. It might be desirable to make
+// this more generic and include it in the TinyGo "machine" package instead.
+type bbSPI struct {
+	SCK   machine.Pin
+	SDI   machine.Pin
+	SDO   machine.Pin
+	Delay uint32
+}
+
+// Configure sets up the SCK and SDO pins as outputs and sets them low
+func (s *bbSPI) Configure() {
+	s.SCK.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	s.SDO.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	s.SCK.Low()
+	s.SDO.Low()
+	if s.Delay == 0 {
+		s.Delay = 1
+	}
+}
+
+// Tx matches signature of machine.SPI.Tx() and is used to send multiple bytes.
+// The r slice is ignored and no error will ever be returned.
+func (s *bbSPI) Tx(w []byte, r []byte) error {
+	s.Configure()
+	for _, b := range w {
+		s.Transfer(b)
+	}
+	return nil
+}
+
+// delay represents a quarter of the clock cycle
+func (s *bbSPI) delay() {
+	for i := uint32(0); i < s.Delay; {
+		i++
+	}
+}
+
+// Transfer matches signature of machine.SPI.Transfer() and is used to send a
+// single byte. The received data is ignored and no error will ever be returned.
+func (s *bbSPI) Transfer(b byte) (byte, error) {
+	for i := uint8(0); i < 8; i++ {
+
+		// half clock cycle high to start
+		s.SCK.High()
+		s.delay()
+
+		// write the value to SDO (MSB first)
+		s.SDO.Set(b&(1<<(7-i)) != 0)
+		// if  {
+		// 	s.SDO.Low()
+		// } else {
+		// 	s.SDO.High()
+		// }
+		s.delay()
+
+		// half clock cycle low
+		s.SCK.Low()
+		s.delay()
+
+		// for actual SPI would try to read the SDI value here
+		s.delay()
+	}
+
+	return 0, nil
+}
--- cy43439.go
+++ cy43439.go
@@ -1,12 +1,13 @@
 package cyw43439
 
 import (
+	"encoding/binary"
 	"machine"
 
 	"tinygo.org/x/drivers"
 )
 
-func PicoWSpi() (spi *machine.SPI, cs machine.Pin) {
+func PicoWSpi() (spi drivers.SPI, cs machine.Pin) {
 	// Raspberry Pi Pico W pin definitions for the CY43439.
 	const (
 		WL_REG_ON = machine.GPIO23
@@ -19,6 +20,12 @@ func PicoWSpi() (spi *machine.SPI, cs machine.Pin) {
 	// Need software spi implementation since Rx/Tx are on same pin.
 	cs = CS
 	cs.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	spi = &bbSPI{
+		SCK:   CLK,
+		SDI:   DATA_IN,
+		SDO:   DATA_OUT,
+		Delay: 1 << 10,
+	}
 	return spi, cs
 }
 
@@ -30,8 +37,30 @@ type Dev struct {
 
 func (d *Dev) Init() error {
 	d.cs.High()
-
 	return nil
 }
 
-func (d *Dev) csSet(b bool) { d.cs.Set(b) }
+func (d *Dev) SPIExchange(w, r []byte) (int, error) {
+	d.cs.Low()
+	err := d.spi.Tx(w, nil)
+	if err != nil {
+		return 0, err
+	}
+}
+
+func (d *Dev) ReadReg(fn, reg uint32, size int) (uint32, error) {
+	var buf [4 * 2]byte
+	binary.BigEndian.PutUint32(buf[:4], make_cmd(false, true, fn, reg, 4))
+
+}
+
+func make_cmd(write, inc bool, fn uint32, addr uint32, sz uint32) uint32 {
+	return b2i(write)<<31 | b2i(inc)<<30 | fn<<28 | (addr&0x1ffff)<<11 | sz
+}
+
+func b2i(b bool) uint32 {
+	if b {
+		return 1
+	}
+	return 0
+}
