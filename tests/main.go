70858ecd09ed5a4d35bffc9b99108f7856a1609c
soypat <graded.sp@gmail.com>
2023-02-05 16:42:53 -0300

    make tests with shared SDI/SDO/IRQ pins like on pico W

--- tests/main.go
+++ tests/main.go
@@ -9,5 +9,6 @@ func main() {
 		mockCS  = machine.GPIO2
 		mockCLK = machine.GPIO3
 	)
-	TestMockCY43439(mockSDO, mockSDI, mockCS, mockCLK)
+	// TestMockCY43439(mockSDO, mockSDI, mockCS, mockCLK)
+	TestCy43439RegistersOnPicoW()
 }
--- tests/test_bbspi.go
+++ tests/test_bbspi.go
@@ -20,23 +20,25 @@ func TestMockCY43439(sdo, sdi, cs, clk machine.Pin) {
 	data := []byte{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0xff}
 	spi := &cyw43439.SPIbb{
 		SCK:   clk,
-		SDI:   sdi,
+		SDI:   sdo,
 		SDO:   sdo,
 		Delay: 20,
 	}
 	spi.Configure()
 	println("creating dev")
-	dev := cyw43439.NewDev(spi, cs, machine.GPIO5, machine.GPIO6, sdi)
+	dev := cyw43439.NewDev(spi, cs, machine.GPIO5, sdo, sdo)
 	dev.Init()
 	println("reading mock register 0x14")
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	dev.RegisterReadUint32(0, 0x14)
 
 	println("writing 0xFEEDBEAD to mock register 0x18")
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 	dev.RegisterWriteUint32(0, 0x18, 0xFEEDBEAD)
 
 	println("writing data slice and reading same amount")
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
+	cs.Low()
 	spi.Tx(data, data)
+	cs.High()
 }
