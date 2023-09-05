c54011a35a9db8425fda16d2ccda059b669c73a1
Scott Feldman <sfeldma@gmail.com>
2023-09-05 16:21:57 -0700

    add cywparse for parsing saleae digital.cvs files

---
+++ cmd/cywparse/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Transaction struct {
+	Data []byte
+}
+
+func main() {
+	fileName := flag.String("file", "default.txt", "Path to the input file")
+	flag.Parse()
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	transactions := parseRecords(records[1:])
+	for _, t := range transactions {
+		fmt.Printf("%s\n", parseTransaction(t.Data))
+	}
+}
+
+func swapBytes(n uint32) uint32 {
+	return (n&0xFF00FF00)>>8 | (n&0x00FF00FF)<<8
+}
+
+var fns = map[uint32]string{
+	0: "bus",
+	1: "backplane",
+	2: "wlan",
+	3: "dma2",
+}
+
+func parseTransaction(data []byte) string {
+	endian := "BE"
+	cmd := binary.BigEndian.Uint32(data[:4])
+	size := cmd & 0x7ff
+	if size == 0 {
+		// size==0 so cmd must be LE?
+		endian = "LE"
+		cmd = swapBytes(binary.LittleEndian.Uint32(data[:4]))
+	}
+
+	write := (cmd & (1 << 31)) >> 31
+	autoinc := (cmd & (1 << 30)) >> 30
+	fn := (cmd & (0x3 << 28)) >> 28
+	addr := (cmd & (0x1ffff << 11)) >> 11
+	size = cmd & 0x7ff
+
+	out := ""
+	if write == 1 {
+		for i := 4; i < len(data) - 4; i+=4 {
+			out += " " + hex.EncodeToString(data[i:i+4])
+		}
+	}
+
+	return fmt.Sprintf("%s cmd=0x%08x addr=0x%05x fn=%-9s sz=0x%03x w=%d inc=%d   %s", endian, cmd, addr, fns[fn], size, write, autoinc, out)
+}
+func parseRecords(records [][]string) []Transaction {
+	var transactions []Transaction
+	var currentTransaction []byte
+	var currentByte uint8
+	var bitCounter uint8
+
+	var prevCS int
+	var prevCLK int
+
+	for _, record := range records {
+		CS, _ := strconv.Atoi(record[1])
+		MockSDI, _ := strconv.Atoi(record[2])
+		CLK, _ := strconv.Atoi(record[3])
+
+		if prevCS == 1 && CS == 0 {
+			// Start of a new transaction
+			currentTransaction = nil
+			currentByte = 0
+			bitCounter = 0
+		}
+
+		if prevCLK == 0 && CLK == 1 {
+			// Capture MockSDI bit on CLK rising edge
+			currentByte = (currentByte << 1) | uint8(MockSDI)
+			bitCounter++
+
+			if bitCounter == 8 {
+				currentTransaction = append(currentTransaction, currentByte)
+				bitCounter = 0
+				currentByte = 0
+			}
+		}
+
+		if prevCS == 0 && CS == 1 {
+			// End of transaction, store the transaction if it has data
+			if len(currentTransaction) > 0 {
+				transactions = append(transactions, Transaction{Data: currentTransaction})
+			}
+		}
+
+		prevCS = CS
+		prevCLK = CLK
+	}
+
+	return transactions
+}
+
