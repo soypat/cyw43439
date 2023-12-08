dd966b2aede1ca4bafbee4c6cebf75066f450545
soypat <graded.sp@gmail.com>
2023-12-07 23:36:02 -0300

    rename cyrw->cyw43439

---
+++ examples/blinky/blinky.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/soypat/cyw43439/cyw43439"
+
+func main() {
+	dev := cyw43439.NewPicoWDevice()
+	cfg := cyw43439.DefaultWifiConfig()
+	// cfg.Logger = logger // Uncomment to see in depth info on wifi device functioning.
+	err := dev.Init(cfg)
+	if err != nil {
+		panic(err)
+	}
+}
