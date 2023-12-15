788f7e1a9cad1ddf36a758b2f00ce63641549faf
Scott Feldman <sfeldma@gmail.com>
2023-12-14 18:20:13 -0800

    add client prog for example/tcpserver (#31)
    * add client prog for example/tcpserver
    
    * add comment on server:port

---
+++ examples/tcpserver/client/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/netip"
+	"time"
+)
+
+const (
+	// Edit to match server's listening TCP addr:port
+	server = "10.0.0.122:1234"
+)
+
+func main() {
+	raddr := netip.MustParseAddrPort(server)
+	conn, err := net.DialTCP("tcp", nil, net.TCPAddrFromAddrPort(raddr))
+	if err != nil {
+		panic(err)
+	}
+	// wait a second for SYN/ACK stuff.
+	time.Sleep(time.Second)
+	dd := make([]byte, 1024)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			n, err := conn.Read(dd)
+			if err != nil {
+				fmt.Println("rerr", err.Error())
+			}
+			if n > 0 {
+				fmt.Printf("read %q\n", string(dd[:n]))
+			}
+		}
+	}()
+	for {
+		_, err = conn.Write([]byte("hello"))
+		if err != nil {
+			fmt.Println("werr", err.Error())
+		}
+		time.Sleep(time.Second)
+	}
+}
